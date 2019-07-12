73a0cca39e7f0e0a6f94db0dd50460e041b26c24
AnthDavis26 <42490176+AnthDavis26@users.noreply.github.com>
2019-07-11 22:30:17 -0700

    Not doing very well. Gotta get it done.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<!--
+Anthony Davis
+CST336
+HW2
+July 12, 2019
+-->
+
+    <head>
+		<meta charset="utf-8" />
+		<title>CST336 - HW2</title>
+		<link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body>
+		<header>
+			<h1><b>This is a test</b></h1>
+		</header>
+		
+		<!-- Main content -->
+		<main>
+		</main>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+</html>
