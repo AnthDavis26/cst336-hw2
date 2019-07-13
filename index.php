d809e802d77058e345a0e5a479669060af6b943f
AnthDavis26 <42490176+AnthDavis26@users.noreply.github.com>
2019-07-12 19:20:25 -0700

    Not doing very well. Gotta get it done.

--- index.php
+++ index.php
@@ -10,20 +10,25 @@ July 12, 2019
     <head>
 		<meta charset="utf-8" />
 		<title>CST336 - HW2</title>
-		<link href="css/styles.css" rel="stylesheet" type="text/css" />
+
+        <link  href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
 
     <body>
 		<header>
-			<h1><b>This is a test</b></h1>
+			<div id="jscript_hello"></div>
+			<div id="css_hello">Hello from css land! If the text is white and the background is black, that is.</div>
 		</header>
-		
-		<!-- Main content -->
-		<main>
-			ye
-		</main>
-        <!-- closing footer -->
+	
         
+		<script src='js/javascript.js'></script>
     </body>
     <!-- closing body -->
 </html>
---
+++ js/javascript.js
@@ -0,0 +1,5 @@
+window.onload = helloWorld();
+
+function helloWorld(){
+	$("#jscript_hello").append("<p>hello from javascript land!</p>");
+}
